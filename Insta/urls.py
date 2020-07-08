70db2c96d4cfbedbd41929163a56c958d1e51abe
fogger9999 <yjzmsu@gmail.com>
2020-07-07 22:37:37 -0400

    render static page

--- Insta/urls.py
+++ Insta/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from Insta.views import HelloDjango
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('insta/', include('Insta.urls'))
+    path('', HelloDjango.as_view(), name='home'),
+
 ]
